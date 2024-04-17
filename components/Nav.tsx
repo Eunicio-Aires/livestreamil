3e0f1353b0b716c2be4ccc756a50f238522fddac
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-04-17 15:10:08 +0200

    tetesajngfd

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,15 +8,15 @@ export default function Nav(){
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
+      <li><a>Perfil</a></li>
       <li>
         <details>
           <summary>
             Parent
           </summary>
           <ul className="p-2 bg-base-100 rounded-t-none">
-            <li><a>Link 1</a></li>
-            <li><a>Link 2</a></li>
+            <li><a>Live</a></li>
+            <li><a>Gravados</a></li>
           </ul>
         </details>
       </li>
