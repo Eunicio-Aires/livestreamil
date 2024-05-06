afa5410a2edc029fae602541e366773d985d3f3f
Eunicio-Aires <92591524+Eunicio-Aires@users.noreply.github.com>
2024-05-06 03:32:29 +0200

    uigjbjhv

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,15 @@ import React,  { SyntheticEvent } from "react";
 
 export default function Home() {
 
-    const handleSubmit = async (event: SyntheticEvent) => {
-    event.preventDefault();
-    const data =  {
-      email: (event.target as HTMLFormElement).email.value,
-      password: (event.target as HTMLFormElement).password.value,
-      };
+//     const handleSubmit = async (event: SyntheticEvent) => {
+//     event.preventDefault();
+//     const data =  {
+//       email: (event.target as HTMLFormElement).email.value,
+//       password: (event.target as HTMLFormElement).password.value,
+//       };
 
-      redirect(`https://livestreamil.vercel.app/tv`)
-   }
+//       redirect(`https://livestreamil.vercel.app/tv`)
+//    }
   return (
  <div>
     <div className="flex items-center min-h-screen bg-white dark:bg-gray-900">
@@ -38,7 +38,7 @@ export default function Home() {
                 <p className="text-gray-500 dark:text-gray-400">Sign in to access your account</p> */}
             </div>
             <div className="m-7">
-            <form onSubmit={handleSubmit}>
+            <form >
                     <div className="mb-6">
                         <label htmlFor="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email </label>
                         <input type="email" name="email" id="email" placeholder="meuEmail@mail.com" className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
@@ -59,7 +59,7 @@ export default function Home() {
 
                     </div>
                     <div className="mb-6">
-                        <button type="submit" className="w-full px-3 py-4 text-white bg-red-500 rounded-md focus:bg-indigo-600 focus:outline-none">Entrar</button>
+                        <button type="button" className="w-full px-3 py-4 text-white bg-red-500 rounded-md focus:bg-indigo-600 focus:outline-none">Entrar</button>
                     </div>
                     <p className="text-sm text-center text-gray-400">Nao tenho uma conta ainda <a href="#!" className="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800">Criar</a>.</p>
                 </form>
